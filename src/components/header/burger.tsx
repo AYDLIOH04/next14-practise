fc2c2662e82f6a0f4521aa4fc8227864e6c7f6e5
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-01-30 22:13:08 +0500

    фикс бага

--- src/components/header/burger.tsx
+++ src/components/header/burger.tsx
@@ -11,7 +11,7 @@ const HeaderBurger = ({ open, setOpen }: HeaderBurgerProps) => {
   };
 
   return (
-    <button className="flex lg:hidden text-3xl rounded-full p-2 bg-white text-main z-[100]" onClick={onBurgerToggle}>
+    <button className="flex lg:hidden text-3xl rounded-full p-2 bg-white text-main z-[100] fixed right-4 top-2" onClick={onBurgerToggle}>
       {open ? <ImCross /> : <ImMenu />}
     </button>
   );
